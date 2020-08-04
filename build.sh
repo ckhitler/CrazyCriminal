61fc3f6951fea0d94433b28213b4077f5b522346
root <root@192.168.33.11>
2020-08-04 16:50:01 +0800

    push

---
+++ build.sh
@@ -0,0 +1,17 @@
+gcc core_dump.c -o core_dump
+gcc cpu_load.c -o cpu_load
+gcc docker_max.c -o docker_max
+gcc file_max.c -o file_max
+gcc fork_crazy.c -o fork_crazy
+gcc iops_load.c -o iops_load
+gcc iptables_drop.c -o iptables_drop
+gcc memory_crazy.c -o memory_crazy
+gcc net_delay.c -o net_delay
+gcc net_down.c -o net_down
+gcc net_loss.c -o net_loss
+gcc procress_crazy.c -o procress_crazy
+gcc sched_fifo.c -o sched_fifo -pthread
+gcc signal_pending.c -o signal_pending
+gcc socket_connect.c -o socket_connect -pthread
+gcc thread_crazy.c -o thread_crazy -pthread
+gcc write_log.c -o write_log
---
+++ thread_crazy.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include<errno.h>
+#include <pthread.h>
+
+#define MAX_PROC 19999
+
+static void *fn(void *p)
+{
+    while (1) sleep(60);
+    return NULL;
+}
+
+int main(void)
+{
+    struct sched_param sp;
+    pthread_t t;
+
+    unsigned long i;
+
+    /* start of test */
+    for (i = 0; i < MAX_PROC; ++i) {
+        if (pthread_create(&t, NULL, fn, NULL) != 0){
+            perror("fail create pthread");
+            exit(1);
+	}
+    }
+
+    sleep(600);
+    exit(0);
+}
